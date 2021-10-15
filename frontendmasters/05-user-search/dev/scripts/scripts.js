b9e66b2509d824844301ef7f0df13a8f26689d33
Jared Joven <jjoven@craftylab.com>
2021-10-15 19:51:14 -0400

    got date working

--- frontendmasters/05-user-search/dev/scripts/scripts.js
+++ frontendmasters/05-user-search/dev/scripts/scripts.js
@@ -32,42 +32,78 @@ function toggleTheme() {
 themeToggle.addEventListener('click', toggleTheme);
 
 // The API stuff
-const endpoint = 'https://api.github.com/users/';
-const startSearch = document.querySelector('.search-form');
-const errorMsg = document.querySelector('.error');
-const userAvatar = document.querySelector('.user-avatar');
-const userName = document.querySelector('.username');
-const userJoinDate = document.querySelector('.join-date');
-const userHandle = document.querySelector('.handle');
-const userBio = document.querySelector('.bio');
-const userRepos = document.querySelector('.repos');
-const userFollowers = document.querySelector('.followers');
-const userFollowing = document.querySelector('.following');
-const userCity = document.querySelector('.city');
-const userTwitter = document.querySelector('.twitter');
-const userSite = document.querySelector('.site-link');
-const userLink = document.querySelector('.github-link');
+let endpoint = 'https://api.github.com/users/';
+let startSearch = document.querySelector('.search-form');
+let errorMsg = document.querySelector('.error');
+let userAvatar = document.querySelector('.user-avatar');
+let userName = document.querySelector('.username');
+let userJoinDate = document.querySelector('.join-date');
+let userHandle = document.querySelector('.handle');
+let userBio = document.querySelector('.bio');
+let userRepos = document.querySelector('.repos');
+let userFollowers = document.querySelector('.followers');
+let userFollowing = document.querySelector('.following');
+let userCity = document.querySelector('.city');
+let userTwitter = document.querySelector('.twitter');
+let userSite = document.querySelector('.site-link');
+let userCompany = document.querySelector('.company');
+let notAvail = 'Not available';
 
 async function getUser(user) {
-	const userSearch = document.getElementById('username').value;
-	const fullString = `${endpoint}${userSearch}`;
-	const theSearch = await fetch(fullString);
-	const data = await theSearch.json();
+	let userSearch = document.getElementById('username').value;
+	let fullString = `${endpoint}${userSearch}`;
+	let theSearch = await fetch(fullString);
+	let data = await theSearch.json();
+	console.table(data);
 	userData(data);
 }
 function userData(user) {
+	userName.innerText = `${user.login}`;
+	userHandle.innerText = `@${user.name}`;
+	userHandle.href = `${user.html_url}`;
+	userRepos.innerText = `${user.public_repos}`;
+	userFollowers.innerText = `${user.followers}`;
+	userFollowing.innerText = `${user.following}`;
+	// get to understand how the slice works in this case
+	let userJoin = new Date(user.created_at).toUTCString().split(' ').slice(1,4,5,6).join(' ');
+	userJoinDate.innerText = `${userJoin}`;
+	console.log(userJoin);
+	if (`${user.bio}` === 'null') {
+		userBio.innerHTML = `<p>No bio provided.</p>`;
+	} else {
+		userBio.innerHTML = `<p>${user.bio}</p>`;
+	}
+	if (`${user.location}` === 'null' || `${user.location}` === 'undefined') {
+		userCity.innerHTML = `${notAvail}`;
+	} else {
+		userCity.innerHTML = `${user.location}`;
+	}
+	if (`${user.twitter_username}` === 'null' || `${user.twitter_username}` === 'undefined') {
+		userTwitter.innerHTML = `${notAvail}`;
+	} else {
+		userTwitter.innerHTML = `${user.twitter_username}`;
+	}
+	if (`${user.blog}` === 'null' || `${user.blog}` === 'undefined' || `${user.blog}` === '') {
+		userSite.innerHTML = `${notAvail}`;
+	} else {
+		userSite.innerHTML = `${user.blog}`;
+	}
+	if (`${user.company}` === 'null' || `${user.company}` === 'undefined' || `${user.company}` === '') {
+		userCompany.innerHTML = `${notAvail}`;
+	} else {
+		userCompany.innerHTML = `${user.company}`;
+	}
+	
 
-		userName.innerText = `${user.login}`;
-
+	
+	
 }
 
 startSearch.addEventListener('submit', (e) => {
 	e.preventDefault();
 	getUser(userName);
 });
-// function displayRepo() {
-// 	const repoData = 
-// }
+
 
 // Call the functions on load
 initTheme();
